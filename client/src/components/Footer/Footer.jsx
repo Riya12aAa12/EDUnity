d62a68f2b252bfb91e7f4caa5a99e88f1ce24935
Riya12aAa12 <rk9709236856@gmail.com>
2024-04-27 00:52:26 +0530

    files added

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-container">
+        <div className='footer-data'>
+          <div className='copyright'>
+           
+          
+          </div>
+          <div className='footer-list'>
+            <ul>
+              <li>Terms and Condition</li>
+              <li>Privacy Policy</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ client/src/pages/About/About.jsx
@@ -0,0 +1,31 @@
+import "../About/About.css"
+function About() {
+  return (
+    <div className="about-container">
+      <div className="about">
+      
+<br></br>
+      <h3 className="about">Our Mission</h3>
+      <h2 className="about"><i>Empowering Education, Connecting Minds</i></h2>
+      <p className="about">Our mission is to provide a centralized platform where students can access a variety of services to enhance their learning journey. From collaborative discussions to personalized mentoring, we aim to create an inclusive space that supports and nurtures the diverse needs of our users.</p>
+      <h3 className="about">What Sets Us Apart</h3>
+      <ul>
+        <li className="ul-about"><b>Comprehensive Services:</b> edUnity is not just a forum; it's a multifaceted platform. From academic discussions to stress relief activities, we offer a wide range of services to cater to various aspects of a student's life.</li>
+        <li className="ul-about"><b>Category-specific Interactions:</b> Tailored to different educational stages, our platform ensures that users engage with content and discussions most relevant to their academic level.</li>
+        <li className="ul-about"><b>Anonymity and Support:</b> We understand the challenges students face, and our Anxiety Helpdesk provides a safe and anonymous space for seeking support from professionals in times of need.</li>
+      </ul>
+      <h3 className="about">How edUnity Works</h3>
+      <ol>
+      <li className="ol-about"><b>Learn:</b> Access a wealth of educational resources, from notes to tutorial videos, curated for your academic success</li>
+      <li className="ol-about"><b>Collaborate:</b> Engage in discussions, share insights, and connect with peers from different regions.</li>
+      <li className="ol-about"><b>Grow:</b> Seek guidance from mentors, relieve stress through interactive activities, and participate in group study forums.</li>
+      
+      </ol>
+      
+      
+
+</div>
+    </div>
+  )
+}
+export default About
